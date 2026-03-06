d8ff0b616ab42dc7aa59e922c78f86690db635b8
Jacob <jackan202@gmail.com>
2026-03-06 09:44:34 +0100

    Add project files.

---
+++ Maskinpark/Maskinpark.Client/Models/Machine.cs
@@ -0,0 +1,11 @@
+﻿namespace Maskinpark.Client.Models;
+
+public class Machine
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsOnline { get; set; }
+    public string StatusText => IsOnline ? "Online" : "Offline";
+    public string LastData { get; set; } = string.Empty;
+    public DateTime LastUpdated { get; set; }
+}
