2c939014c929a9a8932fddb2ed462fa83f2feefa
Jacob <jackan202@gmail.com>
2026-03-09 10:15:20 +0100

    list of machines

--- Maskinpark/Maskinpark.Client/Models/Machine.cs
+++ Maskinpark/Maskinpark.Client/Models/Machine.cs
@@ -5,7 +5,6 @@ public class Machine
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public bool IsOnline { get; set; } = false;
-    public string StatusText => IsOnline ? "Online" : "Offline";
     public string LastData { get; set; } = string.Empty;
     public DateTime LastUpdated { get; set; } = DateTime.Now;
 }
