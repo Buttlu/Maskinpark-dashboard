3c82bd4e342dc0ff2a6c0ab221c068ee0d83f234
Jacob <jackan202@gmail.com>
2026-03-09 09:14:09 +0100

    working* navbar
    *not on mobile and something triggers the .blazor-error-ui

--- Maskinpark/Maskinpark.Client/Models/Machine.cs
+++ Maskinpark/Maskinpark.Client/Models/Machine.cs
@@ -2,10 +2,10 @@
 
 public class Machine
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
-    public bool IsOnline { get; set; }
+    public bool IsOnline { get; set; } = false;
     public string StatusText => IsOnline ? "Online" : "Offline";
     public string LastData { get; set; } = string.Empty;
-    public DateTime LastUpdated { get; set; }
+    public DateTime LastUpdated { get; set; } = DateTime.Now;
 }
