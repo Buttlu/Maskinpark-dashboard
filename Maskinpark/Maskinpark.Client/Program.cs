52f5e8ab5f64c88a327966e966e6ad59e505b924
Jacob <jackan202@gmail.com>
2026-03-12 10:16:26 +0100

    updated client

--- Maskinpark/Maskinpark.Client/Program.cs
+++ Maskinpark/Maskinpark.Client/Program.cs
@@ -1,5 +1,10 @@
+using MaskinPark.Contracts;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<IUnitOfWork>();
+builder.Services.AddScoped<IMachineRepository>();
+builder.Services.AddScoped<IMachineService>();
 
 await builder.Build().RunAsync();
