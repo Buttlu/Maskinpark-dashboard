d671960143f0fe89f9512836e60db5b8214a24f7
Jacob <jackan202@gmail.com>
2026-03-12 10:13:34 +0100

    moved Machine Model and created extension methods to map Entity <-> Dto

---
+++ MaskinPark.Models/Entities/Machine.cs
@@ -0,0 +1,10 @@
+﻿namespace MaskinPark.Models.Entities;
+
+public class Machine
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public bool IsOnline { get; set; } = false;
+    public string LastData { get; set; } = string.Empty;
+    public DateTime LastUpdated { get; set; } = DateTime.Now;
+}
---
+++ MaskinPark.Shared/Extensions/MachineMappingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using MaskinPark.Models.Entities;
+using MaskinPark.Shared.Dtos;
+
+namespace MaskinPark.Shared.Extensions;
+
+public static class MachineMappingExtensions
+{
+    public static Machine ToEntity(this MachineDto machineDto)
+        => new() {
+            Id = machineDto.Id,
+            Name = machineDto.Name,
+            IsOnline = machineDto.IsOnline,
+            LastData = machineDto.LastData,
+            LastUpdated = machineDto.LastUpdated,
+        };
+
+    public static MachineDto ToDto(this Machine machine)
+        => new(
+            Id: machine.Id,
+            Name: machine.Name,
+            IsOnline: machine.IsOnline,
+            LastData: machine.LastData,
+            LastUpdated: machine.LastUpdated
+        );
+}
