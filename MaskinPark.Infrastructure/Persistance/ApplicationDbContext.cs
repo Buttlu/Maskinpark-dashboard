3fbbacd067512e89d8639506d7f94b40ecb2129f
Jacob <jackan202@gmail.com>
2026-03-12 10:16:55 +0100

    added database. Right now doesn't work and isn't used, but will be fixed later

---
+++ MaskinPark.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using MaskinPark.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaskinPark.Infrastructure.Persistance;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Machine> Machines { get; set; }
+}
--- Maskinpark/Maskinpark/Program.cs
+++ Maskinpark/Maskinpark/Program.cs
@@ -1,11 +1,25 @@
-using Maskinpark.Client.Pages;
 using Maskinpark.Components;
+using MaskinPark.Contracts;
+using MaskinPark.Infrastructure.Persistance;
+using MaskinPark.Infrastructure.Repositories;
+using MaskinPark.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IMachineService, MachineService>();
+builder.Services.AddScoped<IMachineRepository, MachineRepository>();
+builder.Services.AddScoped(provider => new Lazy<IMachineRepository>(() => provider.GetRequiredService<IMachineRepository>()));
 
 var app = builder.Build();
 
